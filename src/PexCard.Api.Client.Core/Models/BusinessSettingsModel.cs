8250011ecd3769b9acde327dd0957015b245062e
Chris Karcz <ckarcz@gmail.com>
2021-07-26 11:44:16 -0400

    #47227: Add GET business settings endpoint & models (#37)
    * #47227: add GET business settings endpoint & models
    
    * remove empty line

---
+++ src/PexCard.Api.Client.Core/Models/BusinessSettingsModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PexCard.Api.Client.Core.Models
+{
+    public class BusinessSettingsModel
+    {
+        public List<CardType> SupportedCardType { get; set; }
+        public bool UseBusinessBalance { get; set; }
+        public bool UseRemoteDecision { get; set; }
+        public bool BlockCardDefunding { get; set; }
+    }
+}
