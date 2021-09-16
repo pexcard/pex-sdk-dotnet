095d50746b4cc824de2a9ee494a8d58ec18881f4
Chris Karcz <ckarcz@gmail.com>
2021-09-16 10:43:17 -0400

    add group and cardlist to cardholder details response

---
+++ src/PexCard.Api.Client.Core/Models/CardDetailModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PexCard.Api.Client.Core.Models
+{
+    public class CardDetailModel
+    {
+        public int CardId { get; set; }
+        public DateTime IssuedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Last4CardNumber { get; set; }
+        public string CardStatus { get; set; }
+    }
+}
