570fcae348293bb509b1fe2cc35c3700b97e8065
Chris Karcz <ckarcz@gmail.com>
2021-12-07 11:05:30 -0500

    add get admin profile endpoint: GET /v4/business/myprofile (#45)

---
+++ src/PexCard.Api.Client.Core/Models/AdminPermissionsModel.cs
@@ -0,0 +1,25 @@
+﻿namespace PexCard.Api.Client.Core.Models
+{
+    public class AdminPermissionsModel
+    {
+        public bool ViewAdministration { get; set; }
+
+        public bool AddEditTerminateAdministrator { get; set; }
+
+        public bool RequestDeleteACHTransfer { get; set; }
+
+        public bool EditBusinessProfile { get; set; }
+
+        public bool AddEditTerminateCard { get; set; }
+
+        public bool CreateCardholder { get; set; }
+
+        public bool ManageCardholder { get; set; }
+
+        public bool? ViewCardUsage { get; set; }
+
+        public bool RequestCardFunding { get; set; }
+
+        public bool ModifyTransactionNotes { get; set; }
+    }
+}
---
+++ src/PexCard.Api.Client.Core/Models/GetAdminProfileModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PexCard.Api.Client.Core.Models
+{
+    public class GetAdminProfileModel
+    {
+        public BusinessAdminModel Admin { get; set; }
+    }
+}
