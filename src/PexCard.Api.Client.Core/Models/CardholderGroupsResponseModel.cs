6be42780de3905856830dd5fac9ba47109799d44
Chris Karcz <ckarcz@gmail.com>
2022-03-18 14:03:39 -0400

    fix cardholder response models (#49)

---
+++ src/PexCard.Api.Client.Core/Models/CardholderGroupsResponseModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PexCard.Api.Client.Core.Models
+{
+    public class CardholderGroupsResponseModel
+    {
+        public List<CardholderGroupModel> Groups { get; set; }
+    }
+}
