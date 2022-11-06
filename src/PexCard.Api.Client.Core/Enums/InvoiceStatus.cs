6e2aab0ce464267b00a6a20ef52fa563b3d71af3
vpetryk <vpetryk@softjourn.com>
2022-11-06 15:02:41 +0200

    62905 Aplos: Add new External API endpoints to SDK

---
+++ src/PexCard.Api.Client.Core/Enums/InvoiceStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace PexCard.Api.Client.Core.Enums
+{
+    public enum InvoiceStatus
+    {
+        Open = 1,
+        Closed = 2,
+        Cancelled = 3,
+        Draft = 4,
+    }
+}
---
+++ src/PexCard.Api.Client.Core/Models/InvoiceAllocationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PexCard.Api.Client.Core.Models
+{
+    public class InvoiceAllocationModel
+    {
+        public long InvoiceId { get; set; }
+        public string TagValue { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
