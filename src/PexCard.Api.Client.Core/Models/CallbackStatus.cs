fb7d5cb3cb8180353862a69a4cf922f363a4c93b
Chris Karcz <ckarcz@gmail.com>
2022-12-14 13:06:12 -0500

    Fix Callback Enums (#70)
    * fix enum problems
    
    * .net 6
    
    Co-authored-by: Chris Karcz <ckarcz@pexcard.com>

--- src/PexCard.Api.Client.Core/Models/CallbackStatus.cs
+++ src/PexCard.Api.Client.Core/Models/CallbackStatus.cs
@@ -2,7 +2,7 @@
 {
     public enum CallbackStatus
     {
-        Active,
-        Inactive
+        Active = 1,
+        Inactive = 2
     }
 }
--- src/PexCard.Api.Client.Core/Models/CallbackType.cs
+++ src/PexCard.Api.Client.Core/Models/CallbackType.cs
@@ -1,15 +1,26 @@
-﻿namespace PexCard.Api.Client.Core.Models
+﻿using System.ComponentModel;
+
+namespace PexCard.Api.Client.Core.Models
 {
     public enum CallbackType
     {
-        Card,
-        Cardorder,
-        CardOrderVirtual,
-        CardTokenCreated,
-        AuthRealtime,
-        PinRealtime,
-        DeclineRealtime,
-        ReversalRealtime,
-        SettlementPostedRealtime,
+        [Description("CARD")]
+        Card = 1,
+        [Description("CARDORDER")]
+        Cardorder = 2,
+        [Description("CARD-ORDER-VIRTUAL")]
+        CardOrderVirtual = 3,
+        [Description("CARD-TOKEN-CREATED")]
+        CardTokenCreated = 4,
+        [Description("AUTH-REALTIME")]
+        AuthRealtime = 5,
+        [Description("PIN-REALTIME")]
+        PinRealtime = 6,
+        [Description("DECLINE-REALTIME")]
+        DeclineRealtime = 7,
+        [Description("REVERSAL-REALTIME")]
+        ReversalRealtime = 8,
+        [Description("SETTLEMENT-POSTED-REALTIME")]
+        SettlementPostedRealtime = 9
     }
 }
