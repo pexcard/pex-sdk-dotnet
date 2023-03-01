f9c5cbd8650115194375cf5371071bf33402fe0e
Chris Karcz <ckarcz@pexcard.com>
2023-03-01 13:17:39 -0500

    remove default helper creator

--- src/PexCard.Api.Client.Core/Models/CreateSpendingRulesetRequestModel.cs
+++ src/PexCard.Api.Client.Core/Models/CreateSpendingRulesetRequestModel.cs
@@ -17,32 +17,5 @@
         public bool UsePexAccountBalanceForAuths { get; set; }
 
         public bool UseCustomerAuthDecision { get; set; }
-
-        public static CreateSpendingRulesetRequestModel WithDefaultRules(string name) => new CreateSpendingRulesetRequestModel()
-        {
-            Name = name,
-            DailySpendLimit = 5_000,
-            InternationalAllowed = true,
-            CardNotPresentAllowed = true,
-            CardPresence = Models.CardPresence.OnlyCardPresent,
-            UsePexAccountBalanceForAuths = false,
-            UseCustomerAuthDecision = false,
-            SpendingRulesetCategories = new SpendingRulesetCategoriesModel
-            {
-                AssociationsOrganizationsAllowed = true,
-                AutomotiveDealersAllowed = true,
-                EducationalServicesAllowed = true,
-                EntertainmentAllowed = true,
-                FuelPumpsAllowed = true,
-                GasStationsConvenienceStoresAllowed = true,
-                GroceryStoresAllowed = true,
-                HealthcareChildcareServicesAllowed = true,
-                ProfessionalServicesAllowed = true,
-                RestaurantsAllowed = true,
-                RetailStoresAllowed = true,
-                TravelTransportationAllowed = true,
-                HardwareStoresAllowed = true,
-            }
-        };
-    }
+    };
 }
