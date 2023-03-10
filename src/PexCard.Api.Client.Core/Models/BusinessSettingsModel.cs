00aa60f7de3ca4dacfa41f6dbda96f1c8ed98a50
Chris Karcz <ckarcz@pexcard.com>
2023-03-10 10:18:21 -0500

    update BusinessSettingsModel

--- src/PexCard.Api.Client.Core/Models/BusinessSettingsModel.cs
+++ src/PexCard.Api.Client.Core/Models/BusinessSettingsModel.cs
@@ -8,6 +8,11 @@ namespace PexCard.Api.Client.Core.Models
         public bool UseBusinessBalance { get; set; }
         public bool UseRemoteDecision { get; set; }
         public bool BlockCardDefunding { get; set; }
+        public bool UseCardholderGroup { get; set; }
+        public bool TagsEnabled { get; set; }
+        public bool AllocationTagsEnabled { get; internal set; }
         public FundingSource FundingSource { get; set; }
+        public int? CardLimit { get; set; }
+        public decimal? DefaultDailyLimit { get; internal set; }
     }
 }
