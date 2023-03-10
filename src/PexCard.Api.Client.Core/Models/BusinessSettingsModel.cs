3db5eefc87cd367b70f3546f80e3d8c96a5b33ac
Chris Karcz <ckarcz@pexcard.com>
2023-03-10 10:21:07 -0500

    fix internal set

--- src/PexCard.Api.Client.Core/Models/BusinessSettingsModel.cs
+++ src/PexCard.Api.Client.Core/Models/BusinessSettingsModel.cs
@@ -10,9 +10,9 @@ namespace PexCard.Api.Client.Core.Models
         public bool BlockCardDefunding { get; set; }
         public bool UseCardholderGroup { get; set; }
         public bool TagsEnabled { get; set; }
-        public bool AllocationTagsEnabled { get; internal set; }
+        public bool AllocationTagsEnabled { get; set; }
         public FundingSource FundingSource { get; set; }
         public int? CardLimit { get; set; }
-        public decimal? DefaultDailyLimit { get; internal set; }
+        public decimal? DefaultDailyLimit { get; set; }
     }
 }
