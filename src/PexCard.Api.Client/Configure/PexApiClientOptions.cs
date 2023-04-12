6f3c368d9e92c015148afe7bc071ad9ddaa76c2b
Chris Karcz <ckarcz@pexcard.com>
2023-04-12 16:25:25 -0400

    make success Information level by default

--- src/PexCard.Api.Client/Configure/PexApiClientOptions.cs
+++ src/PexCard.Api.Client/Configure/PexApiClientOptions.cs
@@ -15,7 +15,7 @@ namespace PexCard.Api.Client
         [JsonConverter(typeof(JsonTimeSpanConverter))]
         public TimeSpan Timeout { get; set; } = TimeSpan.FromMinutes(3);
 
-        public LogLevel LogLevelSuccess { get; set; } = LogLevel.Debug;
+        public LogLevel LogLevelSuccess { get; set; } = LogLevel.Information;
 
         public LogLevel LogLevelFailure { get; set; } = LogLevel.Warning;
 
