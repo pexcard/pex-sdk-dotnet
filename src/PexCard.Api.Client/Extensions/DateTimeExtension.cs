470320e78987128d2ef117e561dd775393c5f3bb
Chris Karcz <ckarcz@gmail.com>
2023-08-29 15:54:12 -0400

    ISO8601 DateTime Formats (#93)
    * don't convert timezone. and use ToStringIso8601() for date formats
    
    * ISO8601
    
    ---------
    
    Co-authored-by: Chris Karcz <ckarcz@pexcard.com>

--- src/PexCard.Api.Client/Extensions/DateTimeExtension.cs
+++ src/PexCard.Api.Client/Extensions/DateTimeExtension.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using PexCard.Api.Client.Const;
-
-namespace PexCard.Api.Client.Extensions
+﻿namespace System
 {
     internal static class DateTimeExtension
     {
-        public static DateTime ToTimeZone(this DateTime _this, TimeZoneInfo tz)
+        public static string ToStringISO8601(this DateTime dateTime)
         {
-            return TimeZoneInfo.ConvertTime(_this, tz);
-        }
-
-        public static DateTime ToEst(this DateTime _this)
-        {
-            return _this.ToTimeZone(TimeZoneConst.Est);
-        }
-
-        public static string ToDateTimeString(this DateTime _this)
-        {
-            return _this.ToString("yyyy-MM-ddTHH:mm:ss");
+            return dateTime.ToString("o");
         }
     }
 }
