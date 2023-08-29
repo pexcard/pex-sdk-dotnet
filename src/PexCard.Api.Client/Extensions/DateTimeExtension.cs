740622faaa3d503789a15ec81fe19dbc4c3294ee
Chris Karcz <ckarcz@pexcard.com>
2023-08-29 16:31:51 -0400

    Revert "ISO8601 DateTime Formats (#93)"
    This reverts commit 470320e78987128d2ef117e561dd775393c5f3bb.

--- src/PexCard.Api.Client/Extensions/DateTimeExtension.cs
+++ src/PexCard.Api.Client/Extensions/DateTimeExtension.cs
@@ -1,10 +1,23 @@
-﻿namespace System
+﻿using System;
+using PexCard.Api.Client.Const;
+
+namespace PexCard.Api.Client.Extensions
 {
     internal static class DateTimeExtension
     {
-        public static string ToStringISO8601(this DateTime dateTime)
+        public static DateTime ToTimeZone(this DateTime _this, TimeZoneInfo tz)
         {
-            return dateTime.ToString("o");
+            return TimeZoneInfo.ConvertTime(_this, tz);
+        }
+
+        public static DateTime ToEst(this DateTime _this)
+        {
+            return _this.ToTimeZone(TimeZoneConst.Est);
+        }
+
+        public static string ToDateTimeString(this DateTime _this)
+        {
+            return _this.ToString("yyyy-MM-ddTHH:mm:ss");
         }
     }
 }
