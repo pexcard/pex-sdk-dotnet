5506e64d3514385f7ba4bd909c7648c19ec6eaaa
Chris Karcz <ckarcz@pexcard.com>
2023-09-06 13:44:52 -0400

    don't default app name and version in options

--- src/PexCard.Api.Client/Configure/PexAuthClientOptions.cs
+++ src/PexCard.Api.Client/Configure/PexAuthClientOptions.cs
@@ -8,9 +8,9 @@ namespace PexCard.Api.Client
 {
     public class PexAuthClientOptions
     {
-        public string AppName { get; set; } = "pex-app";
+        public string AppName { get; set; }
 
-        public string AppVersion { get; set; } = "0.0.0";
+        public string AppVersion { get; set; }
 
 #pragma warning disable S1075 // URIs should not be hardcoded
         public Uri BaseUri { get; set; } = new Uri("https://oauth.pexcard.com");
