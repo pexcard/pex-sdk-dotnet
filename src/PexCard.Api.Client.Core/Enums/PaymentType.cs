db65f89436ff687e5c0b04f1aa45166ff9488dc2
vpetryk <vpetryk@softjourn.com>
2023-10-16 16:12:03 +0300

    78165 Update API Client with payment type

---
+++ src/PexCard.Api.Client.Core/Enums/PaymentType.cs
@@ -0,0 +1,14 @@
+﻿namespace PexCard.Api.Client.Core.Enums
+{
+    public enum PaymentType
+    {
+        PEXTransfer = 1,
+        SalesCredit,
+        WriteOff,
+        Reversal,
+        RebateCredit,
+        RebateCreditReversal,
+        SameDayACH,
+        CarryOverCredit
+    }
+}
