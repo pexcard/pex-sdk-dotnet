eb589ec820cf592f1e2fcca980c3efc9a2a049cb
vpetryk <vpetryk@softjourn.com>
2023-10-16 16:29:49 +0300

    78165 Update API Client with payment type (#107)

--- src/PexCard.Api.Client.Core/Models/InvoicePaymentModel.cs
+++ src/PexCard.Api.Client.Core/Models/InvoicePaymentModel.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using PexCard.Api.Client.Core.Enums;
 
 namespace PexCard.Api.Client.Core.Models
 {
     public class InvoicePaymentModel
     {
         public int PaymentId { get; set; }
+        public PaymentType Type { get; set; }
         public decimal Amount { get; set; }
     }
 }
