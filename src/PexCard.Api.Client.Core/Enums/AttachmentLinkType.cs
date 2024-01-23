73d26e8a041701bfb29e82043d27be39e70aa1d8
vpetryk <vpetryk@softjourn.com>
2024-01-23 17:15:43 +0200

    68445 Envelope Compression Issue

---
+++ src/PexCard.Api.Client.Core/Enums/AttachmentLinkType.cs
@@ -0,0 +1,17 @@
+﻿namespace PexCard.Api.Client.Core.Enums
+{
+    /// <summary>
+    /// Type of Attachment Link
+    /// </summary>
+    public enum AttachmentLinkType
+    {
+        /// <summary> LinkUrl </summary>
+        LinkUrl = 0,
+        /// <summary> Full </summary>
+        Full = 1,
+        /// <summary> Thumbnail </summary>
+        Thumbnail = 2,
+        /// <summary> Preview </summary>
+        Preview = 3
+    }
+}
