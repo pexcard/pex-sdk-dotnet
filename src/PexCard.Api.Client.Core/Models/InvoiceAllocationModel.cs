c0771a87f70edaf1d29bff599c8c6642a45f4882
vpetryk <vpetryk@softjourn.com>
2024-02-15 15:03:28 +0200

    84486 PEX SDK: Update Invoice allocations

--- src/PexCard.Api.Client.Core/Models/InvoiceAllocationModel.cs
+++ src/PexCard.Api.Client.Core/Models/InvoiceAllocationModel.cs
@@ -3,7 +3,9 @@
     public class InvoiceAllocationModel
     {
         public long InvoiceId { get; set; }
+        public string TagName { get; set; }
         public string TagValue { get; set; }
         public decimal TotalAmount { get; set; }
+        public string TransactionTypeCategory { get; set; }
     }
 }
