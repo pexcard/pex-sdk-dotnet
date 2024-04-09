e494c6418785972f36c85835b019c4f0b48eb2df
Chris Karcz <ckarcz@gmail.com>
2024-04-09 10:04:57 -0400

    token changes (#117)
    Co-authored-by: Chris Karcz <ckarcz@pexcard.com>

--- src/PexCard.Api.Client.Core/Models/TransactionNoteModel.cs
+++ src/PexCard.Api.Client.Core/Models/TransactionNoteModel.cs
@@ -5,8 +5,18 @@ namespace PexCard.Api.Client.Core.Models
     public class TransactionNoteModel
     {
         public long NoteId { get; set; }
-        public string UserName { get; set; }
         public string NoteText { get; set; }
+
+        public string UserName { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserMiddleName { get; set; }
+        public string UserLastName { get; set; }
         public DateTime NoteDate { get; set; }
+
+        public string UpdatedUserName { get; set; }
+        public string UpdatedUserFirstName { get; set; }
+        public string UpdatedUserMiddleName { get; set; }
+        public string UpdatedUserLastName { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
