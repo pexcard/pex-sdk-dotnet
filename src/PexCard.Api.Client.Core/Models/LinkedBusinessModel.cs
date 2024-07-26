afb32691c95c1aaf3f3f09b9fa7e22d25bb49eb5
Chris Karcz <ckarcz@pexcard.com>
2024-07-26 15:28:43 -0400

    add GET /Business/Linked

---
+++ src/PexCard.Api.Client.Core/Models/LinkedBusinessModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PexCard.Api.Client.Core.Models
+{
+    public class LinkedBusinessModel
+    {
+        public int InstitutionId { get; set; }
+
+        public int BusinessAcctId { get; set; }
+
+        public string BusinessName { get; set; }
+
+        public decimal MinBusinessBalance { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+    }
+}
