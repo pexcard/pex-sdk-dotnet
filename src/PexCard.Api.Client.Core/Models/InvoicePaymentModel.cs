bb0e612a06c706ce75a752e321c89ff56d0e99ae
Chris Karcz <ckarcz@pexcard.com>
2024-08-12 14:16:15 -0400

    add GET invoice payment model props

--- src/PexCard.Api.Client.Core/Models/InvoicePaymentModel.cs
+++ src/PexCard.Api.Client.Core/Models/InvoicePaymentModel.cs
@@ -1,11 +1,18 @@
 ﻿using PexCard.Api.Client.Core.Enums;
+using System;
 
 namespace PexCard.Api.Client.Core.Models
 {
     public class InvoicePaymentModel
     {
         public int PaymentId { get; set; }
+        public int? RequestId { get; set; }
         public PaymentType Type { get; set; }
+        public DateTime DatePaid { get; set; }
+        public string Description { get; internal set; }
+        public long? PaymentInitiatedByUserId { get; set; }
+        public int? CreditCarriedOverFromInvoiceId { get; set; }
         public decimal Amount { get; set; }
+        public bool RejectedByBank { get; set; }
     }
 }
