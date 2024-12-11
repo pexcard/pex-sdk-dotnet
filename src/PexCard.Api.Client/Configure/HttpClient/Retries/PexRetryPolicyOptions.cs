f99f3012606696710f361e3c8e669b75d3b33698
Chris Karcz <ckarcz@pexcard.com>
2024-12-11 16:00:09 -0500

    PexRetryPolicyOptions

--- src/PexCard.Api.Client/Configure/HttpClient/Retries/PexRetryPolicyOptions.cs
+++ src/PexCard.Api.Client/Configure/HttpClient/Retries/PexRetryPolicyOptions.cs
@@ -7,26 +7,18 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static PexRetryPolicyOptions None => new PexRetryPolicyOptions
         {
-            RetryLogLevel = LogLevel.Warning,
+            RetryLogLevel = LogLevel.None,
             TooManyRequests = BackoffRetryPolicy.None,
             Timeouts = BackoffRetryPolicy.None,
             ServerErrors = BackoffRetryPolicy.None,
         };
 
-        public static PexRetryPolicyOptions Default => new PexRetryPolicyOptions
-        {
-            RetryLogLevel = LogLevel.Warning,
-            TooManyRequests = new BackoffRetryPolicy(TimeSpan.FromSeconds(5), 7),
-            Timeouts = new BackoffRetryPolicy(TimeSpan.FromSeconds(1), 2),
-            ServerErrors = new BackoffRetryPolicy(TimeSpan.FromMilliseconds(100), 1),
-        };
-
         public LogLevel RetryLogLevel { get; set; } = LogLevel.Warning;
 
-        public BackoffRetryPolicy TooManyRequests { get; set; } = new BackoffRetryPolicy();
+        public BackoffRetryPolicy TooManyRequests { get; set; } = new BackoffRetryPolicy(TimeSpan.FromSeconds(5), 7);
 
-        public BackoffRetryPolicy Timeouts { get; set; } = new BackoffRetryPolicy();
+        public BackoffRetryPolicy Timeouts { get; set; } = new BackoffRetryPolicy(TimeSpan.FromSeconds(1), 2);
 
-        public BackoffRetryPolicy ServerErrors { get; set; } = new BackoffRetryPolicy();
+        public BackoffRetryPolicy ServerErrors { get; set; } = new BackoffRetryPolicy(TimeSpan.FromMilliseconds(100), 1);
     }
 }
