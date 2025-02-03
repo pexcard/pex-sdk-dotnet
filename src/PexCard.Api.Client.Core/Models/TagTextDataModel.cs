1b961045abb4e13edd49c10519610ce80a5cbecb
Chris Karcz <ckarcz@pexcard.com>
2025-02-03 09:47:15 -0500

    99768 - update text tag with minLength and validationRegex props

--- src/PexCard.Api.Client.Core/Models/TagTextDataModel.cs
+++ src/PexCard.Api.Client.Core/Models/TagTextDataModel.cs
@@ -2,8 +2,12 @@
 {
     public class TagTextDataModel : TagDataModel
     {
+        public int MinLength { get; set; }
+
         public int Length { get; set; }
 
         public TagTextValidationType ValidationType { get; set; }
+
+        public string ValidationRegex { get; set; }
     }
 }
