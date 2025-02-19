d8d76d02ac178f8caed3c38daf27f60ed0cef0c4
OOUdovytskyi <oudovytskyi@gmail.com>
2025-02-19 15:26:48 +0200

    Added date ranges to the GetPayments API (#149)
    Co-authored-by: OlehUdovytskyi <oudovytskyi@softjourn.com>

--- src/PexCard.Api.Client.Core/Models/PaymentListRequestModel.cs
+++ src/PexCard.Api.Client.Core/Models/PaymentListRequestModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using PexCard.Api.Client.Core.Enums;
 
 namespace PexCard.Api.Client.Core.Models
@@ -8,5 +9,13 @@ namespace PexCard.Api.Client.Core.Models
         public List<PaymentStatus> PaymentStatuses { get; set; }
 
         public List<PaymentStatusTrigger> PaymentStatusTriggers { get; set; }
+
+        public DateTime? OutboundAchCreationStartDate { get; set; }
+
+        public DateTime? OutboundAchCreationEndDate { get; set; }
+
+        public DateTime? ExpectedPaymentStartDate { get; set; }
+
+        public DateTime? ExpectedPaymentEndDate { get; set; }
     }
 }
